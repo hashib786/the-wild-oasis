064158b08126700617e34d9da1784bdd7aa287c4
Hashib Raja <hashibraja23@gmail.com>
2023-10-16 16:21:49 +0530

    Created some heading with as props 📝📝

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
-import styled from "styled-components";
 import GlobalStyle from "./styles/GlobalStyles";
 import Button from "./ui/Button";
-
-const H1 = styled.h1`
-  font-size: 2.5rem;
-  font-weight: 700;
-`;
+import Heading from "./ui/Heading";
 
 const App = () => {
   return (
     <>
       <GlobalStyle />
       <div>
-        <H1>Hello World!</H1>
+        <Heading as="h1">Hello World!</Heading>
         <Button onClick={() => alert("checkIn")}>Check In!</Button>
+        <Heading as="h2">Hello World!</Heading>
         <Button onClick={() => alert("checkOut")}>Check Out!</Button>
+        <Heading as="h3">Hello World!</Heading>
       </div>
     </>
   );
