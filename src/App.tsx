c008a904a14d305364a7c97b64ad269d778631f2
Hashib Raja <hashibraja23@gmail.com>
2023-10-16 16:36:00 +0530

    Created styled row components where i can align item with row or column wise

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,29 @@ import GlobalStyle from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Heading from "./ui/Heading";
 import Input from "./ui/Input";
+import Row from "./ui/Row";
 
 const App = () => {
   return (
     <>
       <GlobalStyle />
-      <div>
-        <Heading as="h1">Hello World!</Heading>
-        <Button onClick={() => alert("checkIn")}>Check In!</Button>
-        <Button onClick={() => alert("checkOut")}>Check Out!</Button>
-        <Heading as="h2">Hello World!</Heading>
-        <Heading as="h3">Hello World!</Heading>
-        <Input type="text" />
-        <Input type="text" />
-      </div>
+      <Row type="vertical">
+        <Row type="horizontal">
+          <Heading as="h1">Hello World!</Heading>
+          <div>
+            <Button onClick={() => alert("checkIn")}>Check In!</Button>
+            <Button onClick={() => alert("checkOut")}>Check Out!</Button>
+          </div>
+        </Row>
+        <Row type="vertical">
+          <Heading as="h2">Hello World!</Heading>
+          <Heading as="h3">Hello World!</Heading>
+          <div>
+            <Input type="text" />
+            <Input type="text" />
+          </div>
+        </Row>
+      </Row>
     </>
   );
 };
---
+++ src/ui/Row.tsx
@@ -0,0 +1,25 @@
+import styled, { css } from "styled-components";
+
+// Define the type for the `type` prop
+interface RowProps {
+  type?: "horizontal" | "vertical";
+}
+
+const Row = styled.div<RowProps>`
+  display: flex;
+  ${(props) =>
+    props.type === "horizontal" &&
+    css`
+      flex-direction: row;
+      align-items: center;
+      justify-content: space-between;
+    `}
+  ${(props) =>
+    props.type === "vertical" &&
+    css`
+      flex-direction: column;
+      gap: 1.5rem;
+    `}
+`;
+
+export default Row;
