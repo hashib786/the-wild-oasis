4c2e944ba87232b2793132d328ab18d0bdc9f0e6
Hashib Raja <hashibraja23@gmail.com>
2023-10-16 18:51:24 +0530

    Created in sidebar all NavLinks 🔗🔗

--- src/ui/SideBar.tsx
+++ src/ui/SideBar.tsx
@@ -1,14 +1,25 @@
 import styled from "styled-components";
+import Logo from "./Logo";
+import MainNav from "./MainNav";
 
 const StyledSideBar = styled.aside`
-  padding: 2.4rem 3rem;
+  padding: 3.2rem 2.4rem;
   grid-row: 1 / -1;
   background-color: var(--color-grey-0);
-  border-right: 1px solid var(--color-grey-200);
+  border-right: 1px solid var(--color-grey-100);
+
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
 `;
 
 const SideBar = () => {
-  return <StyledSideBar>SideBar</StyledSideBar>;
+  return (
+    <StyledSideBar>
+      <Logo />
+      <MainNav />
+    </StyledSideBar>
+  );
 };
 
 export default SideBar;
