e5768fb9cd4f5115b318d7194a6992188ed1ec00
Hashib Raja <hashibraja23@gmail.com>
2023-10-19 19:14:12 +0530

    Use Modal component to show add cabin

--- src/features/cabins/AddCabin.tsx
+++ src/features/cabins/AddCabin.tsx
@@ -1,18 +1,20 @@
-import { useState } from "react";
 import Button from "../../ui/Button";
 import CreateCabinForm from "./CreateCabinForm";
 import Modal from "../../ui/Modal";
 
+const { Open, Window } = Modal;
+
 const AddCabin = () => {
-  const [isCreating, setIsCreating] = useState(false);
   return (
     <div>
-      <Button onClick={() => setIsCreating(!isCreating)}>Add New Cabin</Button>
-      {isCreating && (
-        <Modal onClose={() => setIsCreating(false)}>
-          <CreateCabinForm onClose={() => setIsCreating(false)} />
-        </Modal>
-      )}
+      <Modal>
+        <Open opens="add-cabin">
+          <Button>Add New Cabin</Button>
+        </Open>
+        <Window name="add-cabin">
+          <CreateCabinForm />
+        </Window>
+      </Modal>
     </div>
   );
 };
