f171183adb857ba5e5719aea6d736358e8f5be67
Hashib Raja <hashibraja23@gmail.com>
2023-10-24 19:20:02 +0530

    in CabinTableOperations showing short by component

--- src/features/cabins/CabinTableOperations.tsx
+++ src/features/cabins/CabinTableOperations.tsx
@@ -1,17 +1,27 @@
 import Filter from "../../ui/Filter";
+import SortBy from "../../ui/SortBy";
 import TableOperations from "../../ui/TableOperations";
+
+const filterOptions: FilterI[] = [
+  { label: "All", value: "all" },
+  { label: "No discount", value: "no-discount" },
+  { label: "With discount", value: "with-discount" },
+];
+
+const sortOptions: FilterI[] = [
+  { label: "Sort by name (A-Z)", value: "name-asc" },
+  { label: "Sort by name (Z-A)", value: "name-desc" },
+  { label: "Sort by price (low first)", value: "regularPrice-asc" },
+  { label: "Sort by price (high first)", value: "regularPrice-desc" },
+  { label: "Sort by capacity (low first)", value: "maxCapacity-asc" },
+  { label: "Sort by capacity (high first)", value: "maxCapacity-desc" },
+];
 
 const CabinTableOperations = () => {
   return (
     <TableOperations>
-      <Filter
-        filter="discount"
-        filters={[
-          { label: "All", value: "all" },
-          { label: "No discount", value: "no-discount" },
-          { label: "With discount", value: "with-discount" },
-        ]}
-      />
+      <Filter filter="discount" filters={filterOptions} />
+      <SortBy options={sortOptions} />
     </TableOperations>
   );
 };
--- src/ui/SortBy.tsx
+++ src/ui/SortBy.tsx
@@ -1,10 +1,27 @@
+import { useSearchParams } from "react-router-dom";
+import Select from "./Select";
+
 type Props = {
   options: FilterI[];
 };
 
 const SortBy = ({ options }: Props) => {
-  console.log(options);
-  return <div>SortBy</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const currentParams = searchParams.get("sortBy") || "";
+
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    searchParams.set("sortBy", e.target.value);
+    setSearchParams(searchParams);
+  };
+
+  return (
+    <Select
+      options={options}
+      onChange={handleChange}
+      type="white"
+      value={currentParams}
+    />
+  );
 };
 
 export default SortBy;
